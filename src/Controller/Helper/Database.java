c6669c5a780561d4a51b0d420647f484eab25048
HieuNguyen28 <hieunc.bt@gmail.com>
2021-11-23 19:30:27 +0700

    New Project

---
+++ src/Controller/Helper/Database.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller.Helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Database {
+    private static String connectionUrl = "jdbc:sqlserver://222.253.123.70:1433;databaseName=QL_NhaThuoc;user=sa;password=2892002";
+
+
+    public static PreparedStatement preparedStatement(String sql, Object... args) throws SQLException {
+        Connection con = DriverManager.getConnection(connectionUrl);
+        PreparedStatement pstmt = null;
+        if (sql.trim().startsWith("{")) {
+            pstmt = con.prepareCall(sql);
+        } else {
+            pstmt = con.prepareStatement(sql);
+        }
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+        return pstmt;
+    }
+
+    public static void executeUpdate(String sql, Object... args) {
+        try {
+            PreparedStatement pstmt = preparedStatement(sql, args);
+            try {
+                pstmt.executeUpdate();
+            } finally {
+                pstmt.getConnection().close();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static ResultSet executeQuery(String sql, Object... args) {
+        try {
+            PreparedStatement stmt = preparedStatement(sql, args);
+            return stmt.executeQuery();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/GUI/TextField.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+/**
+ *
+ * @author Laxus
+ */
+public class TextField extends JTextField{
+    public TextField() {
+        setBorder(new EmptyBorder(1, 1, 1, 1));
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+        int width = getWidth();
+        int height = getHeight();
+        g2.fillRect(2, height - 1, width - 4, 1);
+    }
+}
