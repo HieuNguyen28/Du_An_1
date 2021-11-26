0243bdddb33e67698fcc6279a9a29b3aa25b848d
LeKiet1127 <lekiet1127@gmail.com>
2021-11-26 19:38:21 +0700

    Finished form Employee

--- src/Controller/Helper/ValidateSupport.java
+++ src/Controller/Helper/ValidateSupport.java
@@ -6,6 +6,9 @@ package Controller.Helper;
 
 import static java.awt.Color.pink;
 import static java.awt.Color.white;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 
@@ -15,14 +18,12 @@ import javax.swing.JTextPane;
  */
 public class ValidateSupport {
 
-    public static boolean isNull(JTextField txt) {
-        return txt.getText().trim().isEmpty();
-    }
-
-    public static boolean isNull(JTextPane txp) {
-        return txp.getText().trim().isEmpty();
-    }
-
+//    public static boolean isNull(JTextField txt) {
+//         txt.setBackground(pink);
+//         Mgsbox.alert(txt.getRootPane(), "Không được để trống " + txt.getName());
+//         return txt.getText().trim().isEmpty();
+//        
+//    }
     public static boolean isNumber(JTextField txt) {
         if (!txt.getText().trim().matches("(\\d+.\\d+)|\\d+")) {
             Mgsbox.error(txt.getRootPane(), "Please enter a number....");
@@ -31,29 +32,58 @@ public class ValidateSupport {
             return true;
         }
     }
+
     public static boolean checkSDT(JTextField txt) {
         txt.setBackground(white);
         String id = txt.getText();
         String rgx = "(086|096|097|098|032|033|034|035|036|037|038|039|089|090|093|070|079|077|078|076|088|091|094|083|084|085|081|082|092|056|058|099|059)[0-9]{7}";
-        if (id.matches(rgx)) {
-            return true;
-        } else {
-            txt.setBackground(pink);
-            Mgsbox.error(txt.getRootPane(),"Number phone invalid.");
-            return false;
-        }
+        return id.matches(rgx);
     }
 
-    public static boolean checkEmail(JTextField txt) {
+    public static boolean isEmail(JTextField txt) {
         txt.setBackground(white);
         String id = txt.getText();
         String rgx = "^[a-zA-Z][a-zA-Z0-9_\\.]{2,32}@[a-zA-Z0-9]{2,10}(\\.[a-zA-Z0-9]{2,4}){1,2}$";
-        if (id.matches(rgx)) {
-            return true;
-        } else {
-            txt.setBackground(pink);
-            Mgsbox.error(txt.getRootPane(), "Email invalid...");
+       return id.matches(rgx);
+    }
+
+    public static boolean checkEmployeeID(JTextField txt) {
+        txt.setBackground(white);
+        String id = txt.getText();
+        String rgx = "[A-Z0-9]{5,10}";
+         return id.matches(rgx);
+    }
+
+    public static boolean isCCCD(JTextField txt) {
+        txt.setBackground(white);
+        String id = txt.getText();
+        String rgx = "[0-9]{12}";
+        return id.matches(rgx);
+    }
+
+    public boolean isNullImage(JLabel label) {
+        if (label.getToolTipText() != null) {
             return false;
+        } else {
+            Mgsbox.error(label.getRootPane(), "Image can't be empty");
+            return true;
         }
+    }
+
+    public static boolean isNull(JTextField txt) {
+        txt.setBackground(white);
+        if (txt.getText().trim().length() > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean isNull(JTextPane txp) {
+        return txp.getText().trim().isEmpty();
+    }
+    
+    public static boolean isSeleted(JRadioButton rdo) {
+        return rdo.isSelected();  
     }
 }
